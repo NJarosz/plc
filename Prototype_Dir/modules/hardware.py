92115cde4363e11d6971e25b73026b36a9d44a07
Nick Laptop <Njarosz1@gmail.com>
2025-04-07 22:38:51 -0500

    Updated import statement for I2C driver in hardware.py

--- Prototype_Dir/modules/hardware.py
+++ Prototype_Dir/modules/hardware.py
@@ -1,6 +1,6 @@
 # hardware.py
 from gpiozero import OutputDevice, Button
-from ..lib import I2C_LCD_driver
+from lib import I2C_LCD_driver
 from mfrc522 import SimpleMFRC522
 from config import RELAY_PINS, BUTTON_PINS
 
