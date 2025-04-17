0aa3cc62a48f6cd8e69352698bb6fc76034e6f5e
Nick Laptop <Njarosz1@gmail.com>
2025-04-16 20:38:06 -0500

    cleaned up variables in txt_to_json

--- Prototype_Dir/txt_to_json.py
+++ Prototype_Dir/txt_to_json.py
@@ -1,9 +1,5 @@
-import sys
-import os
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'Prototype_Dir')))
-
 import json
-from config import EMPLOYEE_INFO_FILE, PRODUCTION_INFO_FILE, TOTAL_COUNT_FILE
+from config import EMPLOYEE_INFO_FILE, PRODUCTION_INFO_FILE, TOTALCOUNT_FILE
 
 
 def convert_to_json(input_file, output_file):
@@ -21,7 +17,7 @@ def convert_to_json(input_file, output_file):
 
 
 if __name__ == "__main__":
-    input_files = [PRODUCTION_INFO_FILE, EMPLOYEE_INFO_FILE, TOTAL_COUNT_FILE]
-    output_files = ["data/production.json", "data/employees.json", "data/totalcount.json"]
+    input_files = [PRODUCTION_INFO_FILE, EMPLOYEE_INFO_FILE, TOTALCOUNT_FILE]
+    output_files = ["./data/production.json", "./data/employees.json", "./data/totalcount.json"]
     for i in range(len(input_files)):
         convert_to_json(input_files[i], output_files[i])
